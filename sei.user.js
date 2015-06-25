c7917e7323ce5977aba628000d551e38cae42151
Paulo <paulo@nadameu.com.br>
2015-06-25 15:22:20 -0300

    Acrescentada Possibilidade de ordenar processos, ocultar preferências

--- sei.user.js
+++ sei.user.js
@@ -3,11 +3,21 @@
 // @namespace   http://nadameu.com.br/sei
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @include     https://sei.trf4.jus.br/sei/controlador.php?*
-// @version     4
+// @version     5
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
+
+const Ordenacao = {
+	PADRAO: 0,
+	NUMERO: 1,
+	TIPO: 2,
+	ANOTACAO: 3,
+	INVERTER: 4,
+	PRIORITARIOS: 8
+};
+
 if (/[?&]acao=procedimento_controlar(&|$)/.test(window.location.search)) {
 	modificarTelaProcessos();
 }
@@ -19,23 +29,35 @@ function modificarTelaProcessos() {
 	alternarExibicaoTipo(usuarioDesejaMostrarTipo());
 	alternarExibicaoAnotacoes(usuarioDesejaMostrarAnotacoes());
 	alternarExibicaoCores(usuarioDesejaMostrarCores());
+	definirOrdenacaoTabelas(usuarioDesejaOrdenarTabelas());
+	alternarOcultacaoFieldset(usuarioDesejaOcultarFieldset());
 }
 
 function adicionarEstilos() {
 	GM_addStyle('table.tabelaProcessos { border-collapse: collapse; } .mostrarTipo table.tabelaProcessos td { border: 0 solid black; border-width: 1px 0; } table.tabelaProcessos td.colAdicional { padding: 0.5em 0.3em; } div.anotacao { background-color: #ffa; } div.anotacao.prioridade { background-color: #faa; font-weight: bold; }');
-	GM_addStyle('.colAdicional, .anotacao, .tipo, .especificacao, .ambos { display: none; } .mostrarAnotacoes .colAdicional, .mostrarTipo .colAdicional { display: table-cell; } .mostrarAnotacoes .anotacao { display: block; } .mostrarAnotacoes .iconeAnotacao { display: none; } .mostrarTipo .tipo, .mostrarTipo .especificacao { display: block; } .mostrarTipo th .tipo, .mostrarAnotacoes th .anotacao, .mostrarTipo.mostrarAnotacoes th .ambos { display: inline; font-weight: bold; } .ocultarCores tr { background: none !important; }');
+	GM_addStyle('.colAdicional, .anotacao, .tipo, .especificacao, .ambos { display: none; } .mostrarAnotacoes .colAdicional, .mostrarTipo .colAdicional { display: table-cell; } .mostrarAnotacoes .anotacao { display: block; } .mostrarAnotacoes .iconeAnotacao { display: none; } .mostrarTipo .tipo, .mostrarTipo .especificacao { display: block; } .mostrarTipo th .tipo, .mostrarAnotacoes th .anotacao, .mostrarTipo.mostrarAnotacoes th .ambos { display: inline; font-weight: bold; } .ocultarCores tr { background: none !important; } .ocultarFieldset fieldset > * { display: none; } .ocultarFieldset fieldset legend { display: inherit; }');
 }
 
 function criarBotoes() {
-	var botaoTipo = criarBotaoTipo();
-	$('#ancMeusProcessos').after('<br/>');
-	$('#ancMeusProcessos').next().after(botaoTipo);
-	var botaoAnotacoes = criarBotaoAnotacoes();
-	botaoTipo.after('<br/>');
-	botaoTipo.next().after(botaoAnotacoes);
-	var botaoCor = criarBotaoCor();
-	botaoAnotacoes.after('<br/>');
-	botaoAnotacoes.next().after(botaoCor);
+	let fragmento = document.createDocumentFragment();
+	let fieldset = document.createElement('fieldset');
+	fieldset.className = 'infraFieldset';
+	fragmento.appendChild(fieldset);
+	let legend = document.createElement('legend');
+	legend.style.fontSize = '1em';
+	legend.appendChild(criarBotao('Ocultar preferências', usuarioDesejaOcultarFieldset(), function() {
+		usuarioDesejaOcultarFieldset(this.checked);
+		alternarOcultacaoFieldset(this.checked);
+	}));
+	fieldset.appendChild(legend);
+	fieldset.appendChild(criarBotaoTipo());
+	fieldset.appendChild(document.createElement('br'));
+	fieldset.appendChild(criarBotaoAnotacoes());
+	fieldset.appendChild(document.createElement('br'));
+	fieldset.appendChild(criarBotaoCor());
+	fieldset.appendChild(document.createElement('br'));
+	fieldset.appendChild(criarBotaoOrdenacao());
+	$('#divInfraAreaTabela').before(fragmento);
 }
 
 function criarBotaoTipo() {
@@ -59,13 +81,36 @@ function criarBotaoCor() {
 	});
 }
 
+function criarBotaoOrdenacao() {
+	var botao = $('<select class="infraSelect" style="display: inline-block; font-size: 1em;"></select>');
+	[
+		{valor: Ordenacao.PADRAO, nome: 'Padrão'},
+		{valor: Ordenacao.NUMERO, nome: 'Ano e número (antigos primeiro)'},
+		{valor: Ordenacao.NUMERO | Ordenacao.INVERTER, nome: 'Ano e número (novos primeiro)'},
+		{valor: Ordenacao.TIPO, nome: 'Tipo, especificação e anotação'},
+		{valor: Ordenacao.ANOTACAO, nome: 'Anotação (somente texto)'},
+		{valor: Ordenacao.ANOTACAO | Ordenacao.PRIORITARIOS, nome: 'Anotação (prioritários primeiro)'}
+	].forEach(function(opcao) {
+		botao.append('<option value="' + opcao.valor + '">' + opcao.nome + '</option>');
+	});
+	botao.val(usuarioDesejaOrdenarTabelas());
+	var label = $('<label class="infraLabelOpcional"></label>');
+	label.append('Ordenação dos processos: ').append(botao);
+	botao.on('change', function() {
+		let valor = this.value | 0;
+		usuarioDesejaOrdenarTabelas(valor);
+		definirOrdenacaoTabelas(valor);
+	});
+	return label.get(0);
+}
+
 function criarBotao(texto, checked, handler) {
-	var botao = $('<input type="checkbox">');
+	var botao = $('<input type="checkbox" class="infraCheckbox"/>');
 	botao.get(0).checked = checked;
-	var label = $('<label></label>');
+	var label = $('<label class="infraLabelOpcional"></label>');
 	label.append(botao).append(' ' + texto);
 	botao.on('change', handler);
-	return label;
+	return label.get(0);
 }
 
 function usuarioDesejaMostrarTipo(value) {
@@ -80,6 +125,14 @@ function usuarioDesejaMostrarCores(value) {
 	return getSetBoolean('mostrarCores', value);
 }
 
+function usuarioDesejaOrdenarTabelas(value) {
+	return getSetInt('ordenarTabelas', value);
+}
+
+function usuarioDesejaOcultarFieldset(value) {
+	return getSetBoolean('ocultarFieldset', value);
+}
+
 function getSetBoolean(name, value) {
 	if (typeof value !== 'undefined') {
 		if (value !== true && value !== false) {
@@ -88,6 +141,16 @@ function getSetBoolean(name, value) {
 		GM_setValue(name, value === true ? 'S' : 'N');
 	}
 	return GM_getValue(name, 'N') === 'S';
+}
+
+function getSetInt(name, value) {
+	if (typeof value !== 'undefined') {
+		if (isNaN(value)) {
+			throw new Error('Valor inválido para "' + name + '": "' + value + '"');
+		}
+		GM_setValue(name, value | 0);
+	}
+	return GM_getValue(name, 0) | 0;
 }
 
 function criarColunasAdicionais() {
@@ -116,7 +179,9 @@ function analisarTipo() {
 		var mouseover = $(link).attr('onmouseover');
 		let [trash, text, title] = /^return infraTooltipMostrar\('(.*)','(.*)'\);$/.exec(mouseover);
 		escreverColunaAdicional(link, '<div class="tipo"><b>' + corrigirHTML(title) + '</b></div>');
-		escreverColunaAdicional(link, '<div class="especificacao">' + corrigirHTML(text) + '</div></td>');
+		if (text !== '') {
+			escreverColunaAdicional(link, '<div class="especificacao">' + corrigirHTML(text) + '</div></td>');
+		}
 		var cor = obterCor(title);
 		$(link).parents('tr').css({background: cor});
 	});
@@ -163,6 +228,10 @@ function obterColuna(elemento) {
 	}
 }
 
+function alternarOcultacaoFieldset(ocultar) {
+	$('body').toggleClass('ocultarFieldset', ocultar);
+}
+
 function alternarExibicaoTipo(exibir) {
 	$('body').toggleClass('mostrarTipo', exibir);
 	if (exibir) {
@@ -183,6 +252,118 @@ function alternarExibicaoAnotacoes(exibir) {
 
 function alternarExibicaoCores(exibir) {
 	$('body').toggleClass('ocultarCores', !exibir);
+}
+
+function definirOrdenacaoTabelas(ordenacao) {
+	$('table.tabelaProcessos').each(function(t, tabela) {
+		let linhas = $(tabela).find('tr[id]');
+		let informacoes = [];
+		
+		linhas.each(function(l, linha) {
+			let $linha = $(linha);
+			let links = $linha.find('a[tabindex]');
+			links.each(function(i, link) {
+				$(link).attr('data-ordem-original', link.tabIndex).attr('tabindex', null);
+			})
+			let link = $linha.find('a[data-ordem-original]')[0], $link = $(link);
+			let informacao = {
+				elemento: linha,
+				id: link.href.match(/\&id_procedimento=(\d+)\&/)[1] | 0,
+				ordemOriginal: $link.attr('data-ordem-original')
+			};
+			['tipo', 'especificacao', 'anotacao', 'prioridade'].forEach(function(dado) {
+				let elementos = $linha.find('.' + dado);
+				let texto;
+				if (elementos.length === 0) {
+					texto = '';
+				} else {
+					texto = elementos[0].textContent;
+				}
+				informacao[dado] = texto.toLocaleLowerCase();
+			});
+			informacoes.push(informacao);
+		});
+		
+		let funcaoOrdenacao;
+		switch (ordenacao & 3) {
+			case Ordenacao.ANOTACAO:
+				if (ordenacao & Ordenacao.PRIORITARIOS) {
+					funcaoOrdenacao = ordenarPorAnotacaoPrioritariosPrimeiro;
+				} else {
+					funcaoOrdenacao = ordenarPorAnotacao;
+				}
+				break;
+
+			case Ordenacao.TIPO:
+				funcaoOrdenacao = ordenarPorTipoEspecificacaoAnotacao;
+				break;
+				
+			case Ordenacao.NUMERO:
+				funcaoOrdenacao = ordenarPorId;
+				break;
+				
+			case Ordenacao.PADRAO:
+			default:
+				funcaoOrdenacao = ordenarPorOrdemPadrao;
+				break;
+		}
+		informacoes.sort(funcaoOrdenacao);
+		if (ordenacao & Ordenacao.INVERTER) {
+			informacoes.reverse();
+		}
+
+		let fragmento = document.createDocumentFragment();
+		informacoes.forEach(function(informacao) {
+			fragmento.appendChild(informacao.elemento);
+		});
+		tabela.tBodies[0].appendChild(fragmento);
+	});
+}
+
+function ordenarPorAnotacao(a, b) {
+	let textoA = a.anotacao === '' ? 'zz' : a.anotacao;
+	let textoB = b.anotacao === '' ? 'zz' : b.anotacao;
+	if (textoA < textoB) {
+		return -1;
+	} else if (textoA > textoB) {
+		return +1;
+	} else {
+		return ordenarPorOrdemPadrao(a, b);
+	}
+}
+
+function ordenarPorAnotacaoPrioritariosPrimeiro(a, b) {
+	let textoA = a.prioridade === '' ? 'zz' : a.prioridade;
+	let textoB = b.prioridade === '' ? 'zz' : b.prioridade;
+	textoA += a.anotacao === '' ? 'zz' : a.anotacao;
+	textoB += b.anotacao === '' ? 'zz' : b.anotacao;
+	if (textoA < textoB) {
+		return -1;
+	} else if (textoA > textoB) {
+		return +1;
+	} else {
+		return ordenarPorOrdemPadrao(a, b);
+	}
+}
+
+function ordenarPorId(a, b) {
+	return a.id - b.id;
+}
+
+function ordenarPorOrdemPadrao(a, b) {
+	return a.ordemOriginal - b.ordemOriginal;
+}
+
+function ordenarPorTipoEspecificacaoAnotacao(a, b) {
+	let textoA = [a.tipo, a.especificacao, a.anotacao].join('');
+	let textoB = [b.tipo, b.especificacao, b.anotacao].join('');
+	if (textoA < textoB) {
+		return -1;
+	} else if (textoA > textoB) {
+		return +1;
+	} else {
+		return ordenarPorOrdemPadrao(a, b);
+	}
 }
 
 function corrigirHTML(texto) {
