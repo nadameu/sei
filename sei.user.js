213165f83fc0752971da739df28321688649222a
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-09-12 13:39:41 -0300

    Correção de erro no Tampermonkey

--- sei.user.js
+++ sei.user.js
@@ -3,8 +3,7 @@
 // @namespace   http://nadameu.com.br/sei
 // @include     https://sei.trf4.jus.br/sei/controlador.php?*
 // @include     https://sei.trf4.jus.br/controlador.php?*
-// @version     13.0.2
-// @run-at      document-end
+// @version     13.0.3
 // ==/UserScript==
     
 const CoresMarcadores = [
